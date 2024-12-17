727251867aaaea263bf6be9ed2f5440532f31841
vitthalhankune <147737678+vitthalhankune@users.noreply.github.com>
2024-12-17 12:31:09 +0000

    variablesAndDatatyepes

---
+++ 01_basics/01_dataTyapes.js
@@ -0,0 +1,30 @@
+let name="Vitthal";
+let age=22;
+let isLoggedIn=false;
+let city;
+let state=null;//data type of null is "Object"
+
+
+console.log(typeof (name));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(city));
+console.log(typeof(state));
+
+
+
+
+
+
+//           @ primitive dataTypes @ 
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalon
+//undefined=>Not initialize
+//symbol=>used in react to find unique 
+
+//        @ Non-Primitive Datatype @
+//Object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=7264042368;
+let accountEmail="vitthal@123";
+var accountPassword="412308";
+accountBranch="Wadki";
+let accountState;
+
+//accountId=2;//Not allowed
+
+accountEmail="hankune@gmail.com";
+accountPassword="721866";
+accountBranch="Wadki";
+
+console.log(accountPassword);
+
+console.table([accountId,accountEmail,accountPassword,accountBranch,accountState])
+
+// note-
+// 1)Not prefer to use var beacuse of block scope or functional scope.
